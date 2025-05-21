c64b543aed7aa1fa75626299ef8d5681ef1f9f5c
TungNT <nguyenthanhtung311@gmail.com>
2025-05-21 08:36:36 +0700

    add put method in file login.js

--- routes/authentications/login.js
+++ routes/authentications/login.js
@@ -9,4 +9,8 @@ router.post('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.put('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 module.exports = router;
